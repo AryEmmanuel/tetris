6d20edd755e5879a3b142957d47cd767b5da8b44
Ary Emmanuel <aryes39@hotmail.com>
2017-04-23 11:45:23 -0600

    Ahora se muestra la pieza que sigue.

--- tetris.js
+++ tetris.js
@@ -1,20 +1,24 @@
 (()=> {
     class Tetris {
-        constructor(width, height, side, canvas) {
+        constructor(width, height, side, canvas, canvas2) {
             this.width = width
             this.height = height + 1
             this.side = side
             this.canvas = canvas
+            this.canvas2 = canvas2
             this.dibujo = this.canvas.getContext("2d")
+            this.dibujo2 = this.canvas2.getContext("2d")
             this.paused = false
             this.over = false
             this.pieces = [IPiece, JPiece, LPiece, OPiece, SPiece, TPiece, ZPiece]
             this.colors = {0: new IPiece().color, 1: new JPiece().color, 2: new LPiece().color, 3: new OPiece().color, 4: new SPiece().color, 5: new TPiece().color, 6: new ZPiece().color}
             this.tablero = []
+            this.tablero2 = []
             this.current_piece
+            this.next_piece
             this.activeCs = []
             this.cuadrado = true
-            this.interval = 50
+            this.interval = 47
             this.duration = 0
             this.score = 0
             this.level = 1
@@ -24,7 +28,7 @@
         init() {
             this.crearTablero(this.width, this.height, this.side)
             this.teclas()
-            this.mover(this.dibujo)
+            this.mover(this.dibujo, this.dibujo2)
         }
 
         // Detecta las teclas
@@ -44,13 +48,21 @@
         }
 
         crearTablero(width, height, side) {
+            this.canvas2.width = width * side
+            this.canvas2.height = side * 4
+
             this.canvas.width = width * side
             this.canvas.height = (height-1) * side
 
             this.tablero = new Array(this.width * this.height)
+            this.tablero2 = new Array(this.width * 4)
 
             for (let i = 0; i < this.tablero.length; i++) {
                 this.tablero[i] = {x:0,y:0,id:0,active:false}
+            }
+
+            for (let i = 0; i < this.tablero2.length; i++) {
+                this.tablero2[i] = {x:0,y:0,id:0}
             }
 
             // Coloca las 'x' y 'y' en cada casilla del tablero
@@ -58,6 +70,12 @@
                 for (let j = 0; j < this.width; j++) {
                     this.tablero[i*this.width+j].x = j // X
                     this.tablero[i*this.width+j].y = i // Y
+                }
+            }
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < this.width; j++) {
+                    this.tablero2[i*this.width+j].x = j // X
+                    this.tablero2[i*this.width+j].y = i // Y
                 }
             }
         }
@@ -82,15 +100,16 @@
         }
 
         // Mueve el juego (si no esta pausado)
-        mover(dibujo) {
+        mover(dibujo, dibujo2) {
             var tetris = this
             
             function move(timestamp) {
                 if (!tetris.paused) tetris.duration++
 
                 if (tetris.duration == tetris.interval && !tetris.paused && !tetris.over) {
+                    tetris.selectNextPiece()
                     tetris.select_piece()
-                    tetris.dibujar(dibujo)
+                    tetris.dibujar(dibujo, dibujo2)
                     if (tetris.current_piece) tetris.fall()
                     tetris.duration = 0
                 }
@@ -102,10 +121,18 @@
             })
         }
 
+        selectNextPiece() {
+            if (!this.next_piece) {
+                this.next_piece = new this.pieces[Math.floor(Math.random() * this.pieces.length)](this.side, this.width, 0)
+            }
+        }
+
         // Selecciona una pieza al azar
         select_piece() {
             if (!this.current_piece) {
-                this.current_piece = new this.pieces[Math.floor(Math.random() * this.pieces.length)](this.side, this.width, 0)
+                this.current_piece = this.next_piece
+                this.next_piece = undefined
+                this.selectNextPiece()
                 find_pos(this) ? this.show_piece() : this.lose()
             }
 
@@ -135,6 +162,20 @@
                     if (this.tablero[j].x == pieza.pos[0][i].x && this.tablero[j].y == pieza.pos[0][i].y) {
                         this.tablero[j] = pieza.pos[0][i]
                         this.activeCs.push(this.tablero[j])
+                    }
+                }
+            }
+            this.showNextPiece()
+        }
+
+        showNextPiece() {
+            var pieza = this.next_piece
+            var m = pieza.first_height * this.width
+
+            for (let i = 0; i < pieza.pos[0].length; i++) {
+                for (let j = 0; j < m-1; j++) {
+                    if (this.tablero2[j].x == pieza.pos[0][i].x && this.tablero2[j].y == pieza.pos[0][i].y) {
+                        this.tablero2[j].id = pieza.pos[0][i].id
                     }
                 }
             }
@@ -318,6 +359,7 @@
                     this.current_piece = undefined
                 }
             }
+            for (let c of this.tablero2) if (c.id) c.id = 0
             this.reducir()
         }
 
@@ -370,6 +412,10 @@
                 case 3000:
                     this.interval = 25
                     this.level = 6
+                    break
+                case 4500:
+                    this.interval = 20
+                    this.level = 7
                 
             }
             nivel.innerText = this.level
@@ -382,8 +428,9 @@
             btn_final.style.display = "initial"
         }
 
-        dibujar(dibujo) {
+        dibujar(dibujo, dibujo2) {
             dibujo.clearRect(0,0,this.canvas.width, this.canvas.height)
+            dibujo2.clearRect(0,0,this.canvas2.width, this.canvas2.height)
             var colors = this.colors
 
             for (let i = this.width; i < this.tablero.length; i++) {
@@ -401,13 +448,29 @@
                     dibujo.closePath()
                 }
             }
+            for (let i = 0; i < this.tablero2.length; i++) {
+                let casilla = this.tablero2[i]
+                if (casilla.id) {
+                    dibujo2.beginPath()
+                    dibujo2.fillStyle = colors[casilla.id-1]
+                    dibujo2.strokeStyle = "white"
+                    if (this.cuadrado) {
+                        dibujo2.fillRect(casilla.x * this.side, (casilla.y+1) * this.side, this.side, this.side)
+                        dibujo2.rect(casilla.x * this.side, (casilla.y+1) * this.side, this.side, this.side)
+                    } else dibujo2.arc(casilla.x * this.side + this.side/2, (casilla.y+1) * this.side + this.side/2, this.side/2, 0, Math.PI*2)
+                    dibujo2.fill()
+                    dibujo2.stroke()
+                    dibujo2.closePath()
+                }
+            }
         }
     }
 
     var game = document.getElementById("game")
 
     var canvas = document.getElementById("lienzo")
-    var juego = new Tetris(10, 18, 30, canvas)
+    var canvas2 = document.getElementById("lienzo_2")
+    var juego = new Tetris(10, 18, 30, canvas, canvas2)
 
     var btn_iniciar = document.getElementById("btn_iniciar")
     btn_iniciar.addEventListener("click", function() {
